febf7ae0562fa147b51e02d44e5854b42e982144
Clark Alesna <clark_alesna@hotmail.com>
2024-02-01 03:48:21 +0800

    feat(spec): Restructured to use Utxorpc.Spec as dependency and added Nuget metadata to be published as Utxorpc.Sdk (#1)
    * - Renamed namespace to Utxorpc.Sdk
    - Basic Client Wrapper
    - Using Utxorpc.Spec package instead of submodule of the spec repo proto files.
    
    * Nuget metadata

---
+++ src/Utxorpc.Sdk/UtxorpcClient.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Net.Client;
+using Utxorpc.Submit.V1;
+using Utxorpc.Sync.V1;
+using Utxorpc.Watch.V1;
+
+namespace Utxorpc.Sdk;
+
+public class UtxorpcClient(string url)
+{
+    public ChainSyncService.ChainSyncServiceClient ChainSyncClient => new(GrpcChannel.ForAddress(url));
+    public WatchService.WatchServiceClient WatchClient => new(GrpcChannel.ForAddress(url));
+    public SubmitService.SubmitServiceClient SubmitClient => new(GrpcChannel.ForAddress(url));
+}
