0cb4814635db79f15d7ec0eeefdfbdd3f97623cf
Clark Alesna <clark_alesna@hotmail.com>
2024-07-16 00:42:45 +0800

    feat: update spec to v0.7.0 (#4)

--- src/Utxorpc.Sdk.Example/Program.cs
+++ src/Utxorpc.Sdk.Example/Program.cs
@@ -1,22 +1,23 @@
 ﻿using Google.Protobuf;
 using Utxorpc.Sdk;
-using Utxorpc.Sync.V1;
+using Utxorpc.V1alpha.Sync;
 
 var utxoRpcClient = new UtxorpcClient("http://localhost:50051");
-var chainSyncClient = utxoRpcClient.ChainSyncClient;
+var chainSyncClient = utxoRpcClient.SyncClient;
 
 var request = new FetchBlockRequest();
 request.Ref.Add(
     new BlockRef
     {
         Hash = ByteString.CopyFrom(
-            Convert.FromHexString("3fc0126deefb17acda42ed941f8e61eaaf58ca72a386f831ccfb0daeb61e6d42")
+            Convert.FromHexString("34c65aba4b299113a488b74e2efe3a3dd272d25b470d25f374b2c693d4386535")
         ),
-        Index = 39822377
+        Index = 54131816
     }
 );
+
 
 var response = await chainSyncClient.FetchBlockAsync(request);
 var blockHash = Convert.ToHexString(response.Block[0].Cardano.Header.Hash.ToByteArray());
 var blockSlot = response.Block[0].Cardano.Header.Slot;
-Console.WriteLine($"Block: {blockHash} Slot: {blockSlot}");
+Console.WriteLine($"Block: {blockHash} Slot: {blockSlot} Block Data: {response.Block[0].Cardano.Body.Tx.Count}");
--- src/Utxorpc.Sdk/UtxorpcClient.cs
+++ src/Utxorpc.Sdk/UtxorpcClient.cs
@@ -1,15 +1,16 @@
 ﻿using Grpc.Net.Client;
-using Utxorpc.Submit.V1;
-using Utxorpc.Sync.V1;
-using Utxorpc.Watch.V1;
-using Utxorpc.Build.V1;
+using Utxorpc.V1alpha.Cardano;
+using Utxorpc.V1alpha.Query;
+using Utxorpc.V1alpha.Submit;
+using Utxorpc.V1alpha.Sync;
+using Utxorpc.V1alpha.Watch;
 
 namespace Utxorpc.Sdk;
 
 public class UtxorpcClient(string url)
 {
-    public ChainSyncService.ChainSyncServiceClient ChainSyncClient => new(GrpcChannel.ForAddress(url));
+    public SyncService.SyncServiceClient SyncClient => new(GrpcChannel.ForAddress(url));
     public WatchService.WatchServiceClient WatchClient => new(GrpcChannel.ForAddress(url));
     public SubmitService.SubmitServiceClient SubmitClient => new(GrpcChannel.ForAddress(url));
-    public LedgerStateService.LedgerStateServiceClient LedgerStateClient => new(GrpcChannel.ForAddress(url));
+    public QueryService.QueryServiceClient QueryClient => new(GrpcChannel.ForAddress(url));
 }
