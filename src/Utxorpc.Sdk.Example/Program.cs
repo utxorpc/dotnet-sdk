a726b585cea1c0014ff73750ed426be910997c98
johnquinnvictaboada <quinnvictaboada@gmail.com>
2024-08-17 23:46:55 +0800

    fix: sdk example for FollowTip usage

--- src/Utxorpc.Sdk.Example/Program.cs
+++ src/Utxorpc.Sdk.Example/Program.cs
@@ -1,23 +1,48 @@
-﻿using Google.Protobuf;
-using Utxorpc.Sdk;
-using Utxorpc.V1alpha.Sync;
+﻿using Utxorpc.Sdk;
+using Utxorpc.Sdk.Models.Enums;
 
-var utxoRpcClient = new UtxorpcClient("http://localhost:50051");
-var chainSyncClient = utxoRpcClient.SyncClient;
+// // Sync.FetchBlock Sample
+// var syncServiceClient = new SyncServiceClient("http://localhost:50051");
 
-var request = new FetchBlockRequest();
-request.Ref.Add(
-    new BlockRef
+// Block block = await syncServiceClient.FetchBlockAsync(
+//     "1dace9bc646e9225251db04ff27397c199b04ec3f83c94cad28c438c3e7eeb50",
+//     67823979
+// );
+
+// Console.WriteLine($"Block: {block.Hash} Slot: {block.Slot} Block Data Length: {block.NativeBytes.Length}");
+
+// Sync.FollowTip Sample
+var syncServiceClient = new SyncServiceClient("http://localhost:50051");
+
+await foreach (var response in syncServiceClient.FollowTipAsync(
+    "1dace9bc646e9225251db04ff27397c199b04ec3f83c94cad28c438c3e7eeb50",
+    67823979))
+{
+    Console.WriteLine("___________________");
+    switch (response.Action)
     {
-        Hash = ByteString.CopyFrom(
-            Convert.FromHexString("34c65aba4b299113a488b74e2efe3a3dd272d25b470d25f374b2c693d4386535")
-        ),
-        Index = 54131816
+        case NextResponseAction.Apply:
+            var applyBlock = response.AppliedBlock;
+            if (applyBlock is not null)
+            {
+                Console.WriteLine($"Apply Block: {applyBlock.Hash} Slot: {applyBlock.Slot}");
+                Console.WriteLine($"Cbor: {Convert.ToHexString(applyBlock.NativeBytes ?? [])}");
+            }
+            break;
+        case NextResponseAction.Undo:
+            var undoBlock = response.UndoneBlock;
+            if (undoBlock is not null)
+            {
+                Console.WriteLine($"Undo Block: {undoBlock.Hash} Slot: {undoBlock.Slot}");
+            }
+            break;
+        case NextResponseAction.Reset:
+            var resetRef = response.ResetRef;
+            if (resetRef is not null)
+            {
+                Console.WriteLine($"Reset to Block: {resetRef.Hash} Slot: {resetRef.Index}");
+            }
+            break;
     }
-);
-
-
-var response = await chainSyncClient.FetchBlockAsync(request);
-var blockHash = Convert.ToHexString(response.Block[0].Cardano.Header.Hash.ToByteArray());
-var blockSlot = response.Block[0].Cardano.Header.Slot;
-Console.WriteLine($"Block: {blockHash} Slot: {blockSlot} Block Data: {response.Block[0].Cardano.Body.Tx.Count}");
+    Console.WriteLine("___________________");
+}
