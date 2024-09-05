3f143850d2cb7239ad1b7dee962fc5af7ee2446c
johnquinnvictaboada <quinnvictaboada@gmail.com>
2024-09-06 00:35:32 +0800

    chore: enabled configs for urls

--- src/Utxorpc.Sdk/QueryServiceClient.cs
+++ src/Utxorpc.Sdk/QueryServiceClient.cs
@@ -3,9 +3,31 @@ using Utxorpc.V1alpha.Query;
 
 namespace Utxorpc.Sdk;
 
-public class QueryServiceClient(string url)
+public class QueryServiceClient
 {
-    private readonly QueryService.QueryServiceClient _client = new(GrpcChannel.ForAddress(url));
+    private readonly QueryService.QueryServiceClient _client;
+
+    public QueryServiceClient(string url, IDictionary<string, string>? headers = null)
+    {
+        var httpClientHandler = new HttpClientHandler();
+        var httpClient = new HttpClient(httpClientHandler);
+        
+        if (headers != null)
+        {
+            foreach (var header in headers)
+            {
+                httpClient.DefaultRequestHeaders.Add(header.Key, header.Value);
+            }
+        }
+
+        var channelOptions = new GrpcChannelOptions
+        {
+            HttpClient = httpClient
+        };
+
+        var channel = GrpcChannel.ForAddress(url, channelOptions);
+        _client = new QueryService.QueryServiceClient(channel);
+    }
 
     // TODO: Implement ReadData, ReadParams, ReadUtxos, SearchUtxos methods
 }
