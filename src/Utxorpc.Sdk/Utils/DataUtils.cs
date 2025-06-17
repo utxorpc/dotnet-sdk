f12ddfe9ed76d144dd2205a5a711db76c44342bf
Hermi <hermitimtim23@gmail.com>
2025-06-18 00:19:11 +0800

    feat: add AnyUtxoData, AddressSearchType, AssetSearchType, TxoRef models and conversion methods in DataUtils

--- src/Utxorpc.Sdk/Utils/DataUtils.cs
+++ src/Utxorpc.Sdk/Utils/DataUtils.cs
@@ -6,6 +6,17 @@ using SpecSyncBlockRef = Utxorpc.V1alpha.Sync.BlockRef;
 using SpecWatchBlockRef = Utxorpc.V1alpha.Watch.BlockRef;
 using Block = Utxorpc.Sdk.Models.Block;
 using Utxorpc.V1alpha.Sync;
+using SpecSubmitTxResponse = Utxorpc.V1alpha.Submit.SubmitTxResponse;
+using SubmitTxResponse = Utxorpc.Sdk.Models.SubmitTxResponse;
+using SpecWaitForTxResponse = Utxorpc.V1alpha.Submit.WaitForTxResponse;
+using WaitForTxResponse = Utxorpc.Sdk.Models.WaitForTxResponse;
+using SpecWatchMempoolResponse = Utxorpc.V1alpha.Submit.WatchMempoolResponse;
+using WatchMempoolResponse = Utxorpc.Sdk.Models.WatchMempoolResponse;
+using SpecWatchTxResponse = Utxorpc.V1alpha.Watch.WatchTxResponse;
+using SpecTxInMempool = Utxorpc.V1alpha.Submit.TxInMempool;
+using TxInMempool = Utxorpc.Sdk.Models.TxInMempool;
+using SpecStage = Utxorpc.V1alpha.Submit.Stage;
+using Stage = Utxorpc.Sdk.Models.Enums.Stage;
 
 namespace Utxorpc.Sdk.Utils;
 
@@ -70,4 +81,59 @@ public static class DataUtils
 
     public static NextResponse CreateResetResponse(BlockRef blockRef) => 
         new(NextResponseAction.Reset, ResetRef: blockRef);
+
+    // Submit service conversion methods
+    public static SubmitTxResponse FromSpecSubmitTxResponse(SpecSubmitTxResponse specResponse)
+    {
+        return new SubmitTxResponse(
+            specResponse.Ref.Select(r => r.ToByteArray()).ToList()
+        );
+    }
+
+    public static WaitForTxResponse FromSpecWaitForTxResponse(SpecWaitForTxResponse specResponse)
+    {
+        return new WaitForTxResponse(
+            specResponse.Ref?.ToByteArray(),
+            FromSpecStage(specResponse.Stage)
+        );
+    }
+
+    public static WatchMempoolResponse FromSpecWatchMempoolResponse(SpecWatchMempoolResponse specResponse)
+    {
+        return new WatchMempoolResponse(
+            FromSpecTxInMempool(specResponse.Tx)
+        );
+    }
+
+    public static TxInMempool? FromSpecTxInMempool(SpecTxInMempool? specTxInMempool)
+    {
+        if (specTxInMempool == null) return null;
+
+        return new TxInMempool(
+            specTxInMempool.Ref?.ToByteArray(),
+            specTxInMempool.NativeBytes?.ToByteArray(),
+            FromSpecStage(specTxInMempool.Stage),
+            specTxInMempool.ParsedStateCase == SpecTxInMempool.ParsedStateOneofCase.Cardano 
+                ? new AnyUtxoData(
+                    specTxInMempool.NativeBytes?.ToByteArray() ?? Array.Empty<byte>(),
+                    null,
+                    specTxInMempool.Cardano
+                ) 
+                : null
+        );
+    }
+
+    public static Stage FromSpecStage(SpecStage specStage)
+    {
+        return specStage switch
+        {
+            SpecStage.Unspecified => Stage.Unspecified,
+            SpecStage.Acknowledged => Stage.Acknowledged,
+            SpecStage.Mempool => Stage.Mempool,
+            SpecStage.Network => Stage.Network,
+            SpecStage.Confirmed => Stage.Confirmed,
+            _ => Stage.Unspecified
+        };
+    }
 }
+
