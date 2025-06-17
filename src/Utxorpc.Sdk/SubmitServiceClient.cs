c13c7c60732b8a4f7c66a04bc436477731598da6
Hermi <hermitimtim23@gmail.com>
2025-06-17 20:26:09 +0800

    feat: implement SubmitTxAsync, WatchMempoolAsync, and WaitForTxAsync methods in SubmitServiceClient

--- src/Utxorpc.Sdk/SubmitServiceClient.cs
+++ src/Utxorpc.Sdk/SubmitServiceClient.cs
@@ -1,4 +1,11 @@
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 using Grpc.Net.Client;
+using Utxorpc.Sdk.Models;
+using Utxorpc.Sdk.Models.Enums;
+using Utxorpc.Sdk.Utils;
+using Utxorpc.V1alpha.Cardano;
 using Utxorpc.V1alpha.Submit;
 
 namespace Utxorpc.Sdk;
@@ -11,7 +18,7 @@ public class SubmitServiceClient
     {
         var httpClientHandler = new HttpClientHandler();
         var httpClient = new HttpClient(httpClientHandler);
-        
+
         if (headers != null)
         {
             foreach (var header in headers)
@@ -27,5 +34,111 @@ public class SubmitServiceClient
 
         var channel = GrpcChannel.ForAddress(url, channelOptions);
         _client = new SubmitService.SubmitServiceClient(channel);
+    }
+
+    public async Task<Models.SubmitTxResponse> SubmitTxAsync(Models.Tx[] txs)
+    {
+        SubmitTxRequest request = new();
+
+        foreach (var key in txs)
+        {
+            var protoRef = new AnyChainTx
+            {
+                Raw = ByteString.CopyFrom(key.Raw)
+            };
+            request.Tx.Add(protoRef);
+        }
+
+        var response = await _client.SubmitTxAsync(request);
+        return DataUtils.FromSpecSubmitTxResponse(response);
+    }
+
+    public async IAsyncEnumerable<Models.WatchMempoolResponse> WatchMempoolAsync(Predicate predicate, FieldMask? fieldMask, [System.Runtime.CompilerServices.EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        WatchMempoolRequest request = new()
+        {
+            Predicate = new()
+            {
+                Match = new()
+                {
+                    Cardano = new()
+                }
+            }
+        };
+
+        switch (predicate)
+        {
+            case AddressPredicate addressPredicate:
+                var addressPattern = new AddressPattern();
+
+                switch (addressPredicate.AddressSearch)
+                {
+                    case AddressSearchType.ExactAddress:
+                        addressPattern.ExactAddress = ByteString.CopyFrom(addressPredicate.Address);
+                        break;
+                    case AddressSearchType.PaymentPart:
+                        addressPattern.PaymentPart = ByteString.CopyFrom(addressPredicate.Address);
+                        break;
+                    case AddressSearchType.DelegationPart:
+                        addressPattern.DelegationPart = ByteString.CopyFrom(addressPredicate.Address);
+                        break;
+                    default:
+                        addressPattern.ExactAddress = ByteString.CopyFrom(addressPredicate.Address);
+                        break;
+                }
+                request.Predicate.Match.Cardano.HasAddress = addressPattern;
+                break;
+
+            case AssetPredicate assetPredicate:
+                var assetPattern = new AssetPattern();
+
+                switch (assetPredicate.AssetSearch)
+                {
+                    case AssetSearchType.PolicyId:
+                        assetPattern.PolicyId = ByteString.CopyFrom(assetPredicate.Asset);
+                        break;
+                    case AssetSearchType.AssetName:
+                        assetPattern.AssetName = ByteString.CopyFrom(assetPredicate.Asset);
+                        break;
+                    default:
+                        assetPattern.AssetName = ByteString.CopyFrom(assetPredicate.Asset);
+                        break;
+                }
+                request.Predicate.Match.Cardano.MovesAsset = assetPattern;
+                break;
+
+            default:
+                throw new ArgumentException($"Unsupported predicate type: {predicate.GetType().Name}");
+        }
+
+        if (fieldMask != null)
+        {
+            request.FieldMask = fieldMask;
+        }
+
+        using AsyncServerStreamingCall<V1alpha.Submit.WatchMempoolResponse>? call = _client.WatchMempool(request, cancellationToken: cancellationToken);
+        await foreach (V1alpha.Submit.WatchMempoolResponse? response in call.ResponseStream.ReadAllAsync(cancellationToken))
+        {
+            yield return DataUtils.FromSpecWatchMempoolResponse(response);
+        }
+    }
+    
+    public async IAsyncEnumerable<Models.WaitForTxResponse> WaitForTxAsync(Models.TxoRef[] txoRefs, [System.Runtime.CompilerServices.EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        WaitForTxRequest request = new();
+        
+        foreach (var txoRef in txoRefs)
+        {
+            if (txoRef.Hash != null)
+            {
+                request.Ref.Add(ByteString.CopyFrom(txoRef.Hash));
+            }
+        }
+        
+        using AsyncServerStreamingCall<V1alpha.Submit.WaitForTxResponse>? call = _client.WaitForTx(request, cancellationToken: cancellationToken);
+        await foreach (V1alpha.Submit.WaitForTxResponse? response in call.ResponseStream.ReadAllAsync(cancellationToken))
+        {
+            yield return DataUtils.FromSpecWaitForTxResponse(response);
+        }
     }
 }
